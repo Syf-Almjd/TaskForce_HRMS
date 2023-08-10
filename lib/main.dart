a545fb60472915549b839038182cbfed3da996af
SaifAlmajd M. H. Almassri <70374999+Syf-Almjd@users.noreply.github.com>
2023-08-10 11:59:09 +0800

    first commit (#1)

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'TaskForce'),
+      home: const MyHomePage(title: 'TaskForce-HRMS'),
     );
   }
 }
